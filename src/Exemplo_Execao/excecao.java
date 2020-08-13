db9cbc994d54ef93edfef091aa96b2befb1dca1c
ricardojcosta <ricardoredes2006@gmail.com>
2020-08-13 20:54:10 -0300

    exemplo1

--- src/Exemplo_Execao/excecao.java
+++ src/Exemplo_Execao/excecao.java
@@ -1,12 +1,26 @@
-
 package Exemplo_Execao;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class excecao {
+
     public static void main(String[] args) {
-        System.out.println("Consegui");
-        
-        System.out.println("Segundo commmit");
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            String[] vetor = sc.nextLine().split(" ");
+            int posicao = sc.nextInt();
+            System.out.println(vetor[posicao]);
+
+        } catch (InputMismatchException e) {
+            System.out.println("Não existe indice com letras");
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Indice inexistente");
+        }
+
+        System.out.println("Fim do prorama");
+
     }
- 
 }
