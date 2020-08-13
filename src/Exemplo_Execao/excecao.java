5370c1e0876bbb7c229bf5f067e8d86b36433bbf
ricardojcosta <ricardoredes2006@gmail.com>
2020-08-13 20:16:17 -0300

    segunda versão

--- src/Exemplo_Execao/excecao.java
+++ src/Exemplo_Execao/excecao.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Exemplo_Execao;
 
-/**
- *
- * @author cado_
- */
+
 public class excecao {
-    
+    public static void main(String[] args) {
+        System.out.println("Consegui");
+        
+        System.out.println("Segundo commmit");
+    }
+ 
 }
